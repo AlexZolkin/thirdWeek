fa9d222453b96d10435e6e7db465650c52da6a04
alexzolkin19 <alexzolkin19@gmail.com>
2017-03-11 02:03:44 +0300

    some fixes commentaries added for all code

--- src/thirdTask/MyLinkedList.java
+++ src/thirdTask/MyLinkedList.java
@@ -6,16 +6,29 @@ import java.util.Iterator;
 /**
  * Created by Алексей on 10.03.2017.
  */
+/*
+* MyLinkedList
+* Contains realisation of MyList interface and nested class Node for
+* elements of the list
+* saves head and size for future usage
+* */
 public class MyLinkedList<T> implements MyList<T> {
     private Node<T> head;
     private int listSize;
 
+    /*
+    * Constructor
+    * initialises head of hte list
+    * */
     public MyLinkedList(){
         head = new Node<>();
-        //head.setNext(null);
-        //head.setPrev(null);
         listSize = 1;
     }
+
+    /*
+    * Function for private usage
+    * returns element of thr list with index given
+    * */
     private Node<T> getNode(int index){
         if (index >= listSize || index < 0)
             return null;
@@ -27,22 +40,40 @@ public class MyLinkedList<T> implements MyList<T> {
         }
         return tmp;
     }
+
+    /*
+    * Getter
+    * returns value of the element with index given
+    * */
     @Override
     public T get(int index) {
         Node<T> node = getNode(index);
         return node == null ? null : node.getValue();
     }
 
+    /*
+    * Getter
+    * returns value of the last element
+    * */
     @Override
     public T get() {
         return get(listSize -1);
     }
 
+    /*
+    * Setter
+    * set value of the last element with item
+    * */
     @Override
     public boolean set(T item) {
         return set(item, listSize - 1);
     }
 
+    /*
+    * Setter
+    * set value of the element with index given
+    * with item
+    * */
     @Override
     public boolean set(T item, int index) {
         Node<T> node = getNode(index);
@@ -53,11 +84,21 @@ public class MyLinkedList<T> implements MyList<T> {
         return false;
     }
 
+    /*
+    * Adds new element
+    * adds new element with item value to the end
+    * of the list
+    * */
     @Override
     public boolean add(T item) {
         return add(item, listSize);
     }
 
+    /*
+    * Adds new element
+    * inserts new element in the position given,
+    * value parameter is equal to item
+    * */
     @Override
     public boolean add(T item, int index) {
         if(item == null || index > listSize || index < 0)
@@ -85,6 +126,12 @@ public class MyLinkedList<T> implements MyList<T> {
         return true;
     }
 
+    /*
+    * Adds new elements
+    * takes every element from the collection given
+    * inserts them to the end
+    * order is saved
+    * */
     @Override
     public boolean addAll(Collection<T> array) {
         Iterator<T> iterator = array.iterator();
@@ -94,6 +141,10 @@ public class MyLinkedList<T> implements MyList<T> {
         return true;
     }
 
+    /*
+    * Delete
+    * deletes all the elements with the value equals with item
+    * */
     @Override
     public boolean delete(T item) {
         boolean flag, controlFlag = false;
@@ -115,6 +166,11 @@ public class MyLinkedList<T> implements MyList<T> {
         return controlFlag;
     }
 
+    /*
+    * Delete
+    * deletes element with index given if
+    * is's value equals with the item
+    * */
     @Override
     public boolean delete(T item, int index) {
         if(getNode(index).getValue().equals(item)){
@@ -126,11 +182,21 @@ public class MyLinkedList<T> implements MyList<T> {
         return false;
     }
 
+    /*
+    * Delete
+    * deletes element with the index given
+    * */
     @Override
     public boolean deleteByInd(int index) {
         return delete(getNode(index).getValue(), index);
     }
 
+    /*
+    * Delete
+    * deletes numerous element between two indexes
+    * beg - starting index
+    * end - final index
+    * */
     @Override
     public boolean deleteAll(int beg, int end) {
         Node<T> nodeBeg, nodeEnd;
@@ -145,6 +211,10 @@ public class MyLinkedList<T> implements MyList<T> {
         return false;
     }
 
+    /*
+    * Delete
+    * deletes all the elements in list, which values array contains
+    * */
     @Override
     public boolean deleteAll(Collection<T> array) {
         boolean flag = false;
@@ -157,6 +227,10 @@ public class MyLinkedList<T> implements MyList<T> {
         return flag;
     }
 
+    /*
+    * Contains
+    * checks, doe's list have elements with same value with item
+    * */
     @Override
     public boolean contains(T item) {
         for(int i=0;i<listSize;i++)
@@ -165,42 +239,82 @@ public class MyLinkedList<T> implements MyList<T> {
         return false;
     }
 
+    /*
+    * Size
+    * returns size of a list
+    * */
     @Override
     public int size() {
         return listSize;
     }
 
-
+    /*
+    * Nested class
+    * contains ref's for next end previous nodes, value field
+    * Type parameter is used
+    * Functionality - discrete element of the list
+    * */
     public class Node<T>{
         private Node<T> next, prev;
         private T value;
 
+        /*
+        * Empty constructor
+        * */
         public Node(){
             next = null;
             prev = null;
             value = null;
         }
+
+        /*
+        * Constructor
+        * Initialises new node with data from old one
+        * */
         public Node(Node<T> node){
             next = node.getNext();
             prev = node.getPrev();
             value = node.getValue();
         }
+
+        /*
+        * Constructor
+        * initialises every field during creating
+        * */
         public Node(Node<T> prev, Node<T> next, T value){
             this.prev = prev;
             this.next = next;
             this.value = value;
         }
 
+        /*
+        * Getter
+        * returns value
+        * */
         public T getValue(){
             return value;
         }
+
+        /*
+        * Checks if there is at least one more element
+        * forward in the list
+        * */
         public boolean hasNext(){
             return next == null ? true : false;
         }
+
+        /*
+        * Checks if there is at least one more element
+        * behind in the list
+        * */
         public boolean hasPrev(){
             return prev == null ? true : false;
         }
 
+        /*
+        * Setter
+        * set's value with item
+        * */
         public boolean setValue(T value){
             if(value == null)
                 return false;
@@ -208,14 +322,26 @@ public class MyLinkedList<T> implements MyList<T> {
             return true;
         }
 
+        /*
+        * Getter
+        * returns ref to the next element
+        * */
         public Node<T> getNext(){
             return next;
         }
 
+        /*
+        * Getter
+        * returns ref to the previous element
+        * */
         public Node<T> getPrev(){
             return prev;
         }
 
+        /*
+        * Setter
+        * set's ref to the next element to the item
+        * */
         public boolean setNext(Node<T> item){
             try{
                 next = item;
@@ -225,6 +351,11 @@ public class MyLinkedList<T> implements MyList<T> {
                 return false;
             }
         }
+
+        /*
+        * Setter
+        * set's ref to the previous element to the item
+        * */
         public boolean setPrev(Node<T> item){
             try{
                 prev = item;
--- test/MyListTest.java
+++ test/MyListTest.java
@@ -8,9 +8,19 @@ import java.util.ArrayList;
 /**
  * Created by Алексей on 10.03.2017.
  */
+/*
+* Test class for MyList Interface
+* Tests all the interface methods
+* uses MyLinkedList for performing tests
+* has myList as field - object of the class with implementation
+* */
 public class MyListTest {
     MyLinkedList<Object> myList;
 
+    /*
+    * Resets myList
+    * Initialises it with different values in cycle
+    * */
     @Before
     public void setUp(){
         myList = new MyLinkedList<>();
@@ -23,11 +33,20 @@ public class MyListTest {
         }
     }
 
+    /*
+    * Adding to the end some elements
+    * */
     @Test
     public void testAddToTheEnd(){
         Assert.assertEquals(2, myList.get(2));
         Assert.assertEquals("1", myList.get(1));
     }
+
+    /*
+    * Test of insertion
+    * checking inserted element
+    * and his new neighbours
+    * */
     @Test
     public void testAddInside(){
         myList.add(10,2);
@@ -35,6 +54,11 @@ public class MyListTest {
         Assert.assertEquals(10, myList.get(2));
         Assert.assertEquals(2, myList.get(3));
     }
+
+    /*
+    * Testing multiple adding
+    * adding an Collection to the end of the list
+    * */
     @Test
     public void testAddCollection(){
         ArrayList<Object> arrayList = new ArrayList<>();
@@ -43,20 +67,41 @@ public class MyListTest {
         myList.addAll(arrayList);
         Assert.assertEquals(20,myList.get());
     }
+
+    /*
+    * Testing getter
+    * getting the last element
+    * */
     @Test
     public void testGetLast(){
         Assert.assertEquals("9",myList.get());
     }
+
+    /*
+    * Testing setter
+    * setting the last element
+    * */
     @Test
     public void testSetLast(){
         myList.set(10);
         Assert.assertEquals(10, myList.get());
     }
+
+    /*
+    * Setting element inside the list
+    * */
     @Test
     public void testSetInside(){
         myList.set("10",2);
         Assert.assertEquals("10", myList.get(2));
     }
+
+    /*
+    * Testing multiple deleting
+    * adding 3 same elements to the different parts
+    * of the list
+    * delete them all with one call
+    * */
     @Test
     public void testDelete(){
         myList.add(11,0);
@@ -68,17 +113,32 @@ public class MyListTest {
         Assert.assertNotEquals(11, myList.get());
         Assert.assertNotEquals(11, myList.get(3));
     }
+
+    /*
+    * Testing deleting by index
+    * */
     @Test
     public void testDeleteByInd(){
         myList.deleteByInd(5);
         Assert.assertEquals(6, myList.get(5));
     }
+
+    /*
+    * testing deleting numerous elements
+    * from one index
+    * till another
+    * */
     @Test
     public void testDeleteRange(){
         myList.deleteAll(1,5);
         Assert.assertEquals(0,myList.get(0));
         Assert.assertEquals(6,myList.get(2));
     }
+
+    /*
+    * Testing deleting an array
+    * deleting all the elements array contains from the list
+    * */
     @Test
     public void testDeleteAll(){
         ArrayList<Object> array = new ArrayList<>();
@@ -89,6 +149,11 @@ public class MyListTest {
         Assert.assertEquals("1", myList.get(1));
         Assert.assertEquals("3",myList.get(2));
     }
+
+    /*
+    * testing Contains
+    * checks if list contains elemtnts with value given
+    * */
     @Test
     public void testContains(){
         Assert.assertTrue(myList.contains("1"));
